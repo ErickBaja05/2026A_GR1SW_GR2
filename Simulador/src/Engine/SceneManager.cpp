0b80b806a608243b96f88efee2cbd5de163a696e
Josué <jjccabduscan@gmail.com>
2026-07-15 14:14:33 -0500

    Assign IDs to multiple point lights in SceneManager

--- Simulador/src/Engine/SceneManager.cpp
+++ Simulador/src/Engine/SceneManager.cpp
@@ -365,86 +365,103 @@ void SceneManager::renderDoors(const std::vector<Interactable*>& interactables, 
 
 void SceneManager::setupHouseLights() {
     PointLight* focoBath = new PointLight();
+    focoBath->setId(HouseInteractableIds::Light_Bath); // <--- Asignar ID
     focoBath->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoBath->setPosition(glm::vec3(262.692f, 6.29839f, -5.6183f));
     lightManager->addPointLight(focoBath);
 
     PointLight* focoBed = new PointLight();
+    focoBed->setId(HouseInteractableIds::Light_Bed);
     focoBed->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoBed->setPosition(glm::vec3(263.338f, 6.37f, 1.56341f));
     lightManager->addPointLight(focoBed);
 
     PointLight* focoCocina1 = new PointLight();
+    focoCocina1->setId(HouseInteractableIds::Light_Cocina1);
     focoCocina1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCocina1->setPosition(glm::vec3(270.563f, 2.84878f, -5.3348f));
     lightManager->addPointLight(focoCocina1);
 
     PointLight* focoCocina2 = new PointLight();
+    focoCocina2->setId(HouseInteractableIds::Light_Cocina2);
     focoCocina2->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCocina2->setPosition(glm::vec3(270.187f, 6.29f, -0.825975f));
     lightManager->addPointLight(focoCocina2);
 
     PointLight* focoCV1P1 = new PointLight();
+    focoCV1P1->setId(HouseInteractableIds::Light_CV1_P1);
     focoCV1P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV1P1->setPosition(glm::vec3(264.525f, 2.8753f, 1.73852f));
     lightManager->addPointLight(focoCV1P1);
 
     PointLight* focoCV2P1 = new PointLight();
+    focoCV2P1->setId(HouseInteractableIds::Light_CV2_P1);
     focoCV2P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV2P1->setPosition(glm::vec3(261.209f, 2.59878f, -3.69f));
     lightManager->addPointLight(focoCV2P1);
 
     PointLight* focoCV3P1 = new PointLight();
+    focoCV3P1->setId(HouseInteractableIds::Light_CV3_P1);
     focoCV3P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV3P1->setPosition(glm::vec3(263.78f, 2.81878f, -5.60954f));
     lightManager->addPointLight(focoCV3P1);
 
     PointLight* focoCV4P1 = new PointLight();
+    focoCV4P1->setId(HouseInteractableIds::Light_CV4_P1);
     focoCV4P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV4P1->setPosition(glm::vec3(266.462f, 2.86878f, -4.77572f));
     lightManager->addPointLight(focoCV4P1);
 
     PointLight* focoCV5P1 = new PointLight();
+    focoCV5P1->setId(HouseInteractableIds::Light_CV5_P1);
     focoCV5P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV5P1->setPosition(glm::vec3(266.482f, 2.86878f, -7.26744f));
     lightManager->addPointLight(focoCV5P1);
 
     PointLight* focoCV6P1 = new PointLight();
+    focoCV6P1->setId(HouseInteractableIds::Light_CV6_P1);
     focoCV6P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV6P1->setPosition(glm::vec3(261.15f, 2.59878f, -6.32682f));
     lightManager->addPointLight(focoCV6P1);
 
     PointLight* focoCV8P1 = new PointLight();
+    focoCV8P1->setId(HouseInteractableIds::Light_CV8_P1);
     focoCV8P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV8P1->setPosition(glm::vec3(254.419f, 2.7353f, -4.53488f));
     lightManager->addPointLight(focoCV8P1);
 
     PointLight* focoCV9P1 = new PointLight();
+    focoCV9P1->setId(HouseInteractableIds::Light_CV9_P1);
     focoCV9P1->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV9P1->setPosition(glm::vec3(257.855f, 2.7353f, -4.60542f));
     lightManager->addPointLight(focoCV9P1);
 
     PointLight* focoGarage = new PointLight();
+    focoGarage->setId(HouseInteractableIds::Light_Garage);
     focoGarage->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoGarage->setPosition(glm::vec3(256.109f, 2.7353f, 0.15f));
     lightManager->addPointLight(focoGarage);
 
     PointLight* focoCV1P2 = new PointLight();
+    focoCV1P2->setId(HouseInteractableIds::Light_CV1_P2);
     focoCV1P2->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV1P2->setPosition(glm::vec3(261.319f, 6.35819f, -1.89796f));
     lightManager->addPointLight(focoCV1P2);
 
     PointLight* focoCV2P2 = new PointLight();
+    focoCV2P2->setId(HouseInteractableIds::Light_CV2_P2);
     focoCV2P2->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV2P2->setPosition(glm::vec3(270.03f, 6.35f, -6.18294f));
     lightManager->addPointLight(focoCV2P2);
 
     PointLight* focoCV3P3 = new PointLight();
+    focoCV3P3->setId(HouseInteractableIds::Light_CV3_P3);
     focoCV3P3->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV3P3->setPosition(glm::vec3(266.578f, 6.35819f, -7.61872f));
     lightManager->addPointLight(focoCV3P3);
 
     PointLight* focoCV4P2 = new PointLight();
+    focoCV4P2->setId(HouseInteractableIds::Light_CV4_P2);
     focoCV4P2->properties.diffuse = glm::vec3(1.0f, 0.9f, 0.8f);
     focoCV4P2->setPosition(glm::vec3(266.564f, 6.33819f, -4.96717f));
     lightManager->addPointLight(focoCV4P2);
